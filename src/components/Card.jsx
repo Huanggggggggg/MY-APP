45b39cc4e3a07d9fa0fa0541ae50d5497a23b38b
huang <B11053022@gemail.yuntech.edu.tw>
2024-06-07 16:42:01 +0800

    2024 06 07

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+/**
+ * @auhor Chris Huang <asd978645312@gmail.com>
+ */
+
+
+export default function CustomCard({item}){
+    return(
+        <div class="border">
+            <dev>
+                {item.ScenicSpotName}
+            </dev>
+        </div>
+    )
+}
